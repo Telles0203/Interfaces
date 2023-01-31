7764c787e16904112f8b40f337e989819a3627ed
Alexandre M Telles <alexandre.mtelles@gmail.com>
2023-01-31 13:27:11 -0300

    Exercise - Version 1

---
+++ ExerciseI/Entities/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace ExerciseI.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ ExerciseI/Services/RentalService.cs
@@ -0,0 +1,34 @@
+﻿using ExerciseI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseI.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; set; }
+        public double PricePerDay { get; set; }
+
+        private BrasilTaxService _brasilTaxService = new BrasilTaxService();
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = (duration.TotalHours <= 12.00) ? basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours) : basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+
+            double tax = _brasilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
